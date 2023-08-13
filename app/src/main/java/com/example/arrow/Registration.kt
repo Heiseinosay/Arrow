8a24c1db36c9c92aae2ed5b0929626166998b9c6
Mark-Asuncion <mark.asuncion.000@gmail.com>
2023-08-13 20:54:40 +0800

    use auth

--- app/src/main/java/com/example/arrow/Registration.kt
+++ app/src/main/java/com/example/arrow/Registration.kt
@@ -8,10 +8,15 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.firestore.ktx.firestore
 
 class Registration : AppCompatActivity() {
+    private lateinit var db: FirebaseFirestore
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
@@ -22,7 +27,8 @@ class Registration : AppCompatActivity() {
             startActivity(openLogin)
         }
 
-        val db = Firebase.firestore
+        db = Firebase.firestore
+        auth = Firebase.auth
 
         val getStartedBtn = findViewById<Button>(R.id.btnSignUp)
         getStartedBtn.setOnClickListener {
@@ -31,25 +37,40 @@ class Registration : AppCompatActivity() {
             val pass = findViewById<EditText>(R.id.tvPassword).text.toString().trim()
 
             if (email.isEmpty() || name.isEmpty() || pass.isEmpty()) {
-                Toast.makeText(applicationContext,"Fields cannot be empty",Toast.LENGTH_SHORT)
+                Toast.makeText(applicationContext,"Fields cannot be empty",Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
-            // TODO: check for empty inputs
-            val newUser = hashMapOf(
-                "email" to email,
-                "name" to name,
-                "password" to pass,
-            )
 
-            db.collection("users")
-                .add(newUser)
-                .addOnSuccessListener { documentReference ->
-                    Log.d("FIREBASE_LOG", "DocumentSnapshot added with ID: ${documentReference.id}")
-                }
-                .addOnFailureListener { e ->
-                    Log.w("FIREBASE_LOG", "Error adding document", e)
+            auth.createUserWithEmailAndPassword(email, pass)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Log.d("FIREBASE_AUTH_LOG", "createUserWithEmail:success")
+                        val user = auth.currentUser
+
+//                        val newUser = hashMapOf(
+//                            "email" to email,
+//                            "name" to name,
+//                            "password" to pass,
+//                        )
+//                        db.collection("users")
+//                            .add(newUser)
+//                            .addOnSuccessListener { documentReference ->
+//                                Log.d("FIREBASE_LOG", "DocumentSnapshot added with ID: ${documentReference.id}")
+//                            }
+//                            .addOnFailureListener { e ->
+//                                Log.w("FIREBASE_LOG", "Error adding document", e)
+//                            }
+
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w("FIREBASE_AUTH_LOG", "createUserWithEmail:failure", task.exception)
+                        Toast.makeText(
+                            baseContext,
+                            "Authentication failed.",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                    }
                 }
         }
-
     }
 }
