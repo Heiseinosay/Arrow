b655aabc45279f83c929eac25d6086799fd981cc
Mark-Asuncion <mark.asuncion.000@gmail.com>
2023-08-13 19:52:09 +0800

    bakit ayaw

--- app/src/main/java/com/example/arrow/Registration.kt
+++ app/src/main/java/com/example/arrow/Registration.kt
@@ -7,10 +7,14 @@ import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuthException
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.firestore.ktx.firestore
 
-class Registration : AppCompatActivity() {
+abstract class Registration : AppCompatActivity() {
+    lateinit var db: FirebaseFirestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
@@ -21,22 +25,28 @@ class Registration : AppCompatActivity() {
             startActivity(openLogin)
         }
 
-        val db = Firebase.firestore
+        db = Firebase.firestore
 
         val getStartedBtn = findViewById<Button>(R.id.btnSignUp)
         getStartedBtn.setOnClickListener {
             val email: String = findViewById<EditText>(R.id.tvEmail).text.toString().trim()
             val name = findViewById<EditText>(R.id.tvFullName).text.toString().trim()
             val pass = findViewById<EditText>(R.id.tvPassword).text.toString().trim()
+//            val phone = findViewById<EditText>(R.id.tvPhone).text.toString().trim()
 
+            /*
+            TODO: give temp permission to add user
+            change firestore rules to only allow verified user
+             */
             if (email.isEmpty() || name.isEmpty() || pass.isEmpty()) {
+                Toast.makeText(applicationContext,"Fields must not be empty",Toast.LENGTH_SHORT)
                 return@setOnClickListener
             }
-            // TODO: check for empty inputs
             val newUser = hashMapOf(
                 "email" to email,
                 "name" to name,
                 "password" to pass,
+//                "phone" to phone,
             )
 
             db.collection("users")
