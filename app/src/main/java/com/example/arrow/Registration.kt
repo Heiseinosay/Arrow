5b69c7bf7fd167b65a4ea3c31464e6ba99e24d6f
Mark-Asuncion <mark.asuncion.000@gmail.com>
2023-08-13 20:27:52 +0800

    revert

--- app/src/main/java/com/example/arrow/Registration.kt
+++ app/src/main/java/com/example/arrow/Registration.kt
@@ -8,13 +8,10 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
-import com.google.firebase.auth.FirebaseAuthException
-import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.firestore.ktx.firestore
 
-abstract class Registration : AppCompatActivity() {
-    lateinit var db: FirebaseFirestore
+class Registration : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
@@ -25,28 +22,23 @@ abstract class Registration : AppCompatActivity() {
             startActivity(openLogin)
         }
 
-        db = Firebase.firestore
+        val db = Firebase.firestore
 
         val getStartedBtn = findViewById<Button>(R.id.btnSignUp)
         getStartedBtn.setOnClickListener {
             val email: String = findViewById<EditText>(R.id.tvEmail).text.toString().trim()
             val name = findViewById<EditText>(R.id.tvFullName).text.toString().trim()
             val pass = findViewById<EditText>(R.id.tvPassword).text.toString().trim()
-//            val phone = findViewById<EditText>(R.id.tvPhone).text.toString().trim()
 
-            /*
-            TODO: give temp permission to add user
-            change firestore rules to only allow verified user
-             */
             if (email.isEmpty() || name.isEmpty() || pass.isEmpty()) {
-                Toast.makeText(applicationContext,"Fields must not be empty",Toast.LENGTH_SHORT)
+                Toast.makeText(applicationContext,"Fields cannot be empty",Toast.LENGTH_SHORT)
                 return@setOnClickListener
             }
+            // TODO: check for empty inputs
             val newUser = hashMapOf(
                 "email" to email,
                 "name" to name,
                 "password" to pass,
-//                "phone" to phone,
             )
 
             db.collection("users")
