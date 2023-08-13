2632b63104af9c2ee3f1fb3a7705086e9ad6d88f
E-Museo <e.museongpilipinas@gmail.com>
2023-08-13 20:39:11 +0800

    dependencies

--- app/src/main/java/com/example/arrow/Login.kt
+++ app/src/main/java/com/example/arrow/Login.kt
@@ -1,12 +1,17 @@
 package com.example.arrow
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class Login : AppCompatActivity() {
 
