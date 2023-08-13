4ce5cb6c9ceac6bdf9f467bf696f4e0aac468c4c
Mark-Asuncion <mark.asuncion.000@gmail.com>
2023-08-13 18:13:42 +0800

    line 24 start ng firestore

--- app/src/main/java/com/example/arrow/Registration.kt
+++ app/src/main/java/com/example/arrow/Registration.kt
@@ -3,9 +3,12 @@ package com.example.arrow
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
-import org.w3c.dom.Text
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.firestore.ktx.firestore
 
 class Registration : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,9 +21,32 @@ class Registration : AppCompatActivity() {
             startActivity(openLogin)
         }
 
+        val db = Firebase.firestore
+
         val getStartedBtn = findViewById<Button>(R.id.btnSignUp)
         getStartedBtn.setOnClickListener {
+            val email: String = findViewById<EditText>(R.id.tvEmail).text.toString().trim()
+            val name = findViewById<EditText>(R.id.tvFullName).text.toString().trim()
+            val pass = findViewById<EditText>(R.id.tvPassword).text.toString().trim()
 
+            if (email.isEmpty() || name.isEmpty() || pass.isEmpty()) {
+                return@setOnClickListener
+            }
+            // TODO: check for empty inputs
+            val newUser = hashMapOf(
+                "email" to email,
+                "name" to name,
+                "password" to pass,
+            )
+
+            db.collection("users")
+                .add(newUser)
+                .addOnSuccessListener { documentReference ->
+                    Log.d("FIREBASE_LOG", "DocumentSnapshot added with ID: ${documentReference.id}")
+                }
+                .addOnFailureListener { e ->
+                    Log.w("FIREBASE_LOG", "Error adding document", e)
+                }
         }
 
     }
