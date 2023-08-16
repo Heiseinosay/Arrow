410bb424ac5a7c5a713a75f1bcfa3be30c264f7d
Mark-Asuncion <mark.asuncion.000@gmail.com>
2023-08-16 22:12:02 +0800

    fix errors

--- app/src/main/java/com/example/arrow/RegistrationSecondPart.kt
+++ app/src/main/java/com/example/arrow/RegistrationSecondPart.kt
@@ -23,6 +23,7 @@ class RegistrationSecondPart : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
     private var user: FirebaseUser? = null
     private var classStatus = -1
+    private lateinit var role: String
     @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -63,9 +64,9 @@ class RegistrationSecondPart : AppCompatActivity() {
                 "email" to eMail!!,
                 "date" to dateFormatted!!,
                 "time" to timeFormatted!!,
-                "role" to ,
+                "role" to role,
             )
-            createAccount(newUser)
+            createAccount(newUser, password)
         }
     }
 
@@ -78,6 +79,7 @@ class RegistrationSecondPart : AppCompatActivity() {
         classStatus = status
         when (status) {
             0 -> {
+                role = "Student"
                 student.setCardBackgroundColor(resources.getColor(R.color.darkGrey))
                 employee.setCardBackgroundColor(resources.getColor(R.color.white))
                 visitor.setCardBackgroundColor(resources.getColor(R.color.white))
@@ -86,6 +88,7 @@ class RegistrationSecondPart : AppCompatActivity() {
                 txtVisitor.setTextColor(resources.getColor(R.color.black))
 
             } 1 -> {
+                role = "Employee"
                 employee.setCardBackgroundColor(resources.getColor(R.color.darkGrey))
                 student.setCardBackgroundColor(resources.getColor(R.color.white))
                 visitor.setCardBackgroundColor(resources.getColor(R.color.white))
@@ -93,6 +96,7 @@ class RegistrationSecondPart : AppCompatActivity() {
                 txtEmployee.setTextColor(resources.getColor(R.color.white))
                 txtVisitor.setTextColor(resources.getColor(R.color.black))
             } 2 -> {
+                role = "Guest"
                 visitor.setCardBackgroundColor(resources.getColor(R.color.darkGrey))
                 student.setCardBackgroundColor(resources.getColor(R.color.white))
                 employee.setCardBackgroundColor(resources.getColor(R.color.white))
@@ -105,14 +109,40 @@ class RegistrationSecondPart : AppCompatActivity() {
             }
         }
     }
-    fun createAccount(newUser: HashMap<String, String>) {
+    fun createAccount(newUser: HashMap<String, String>, pass: String?) {
         val email = newUser["email"]
-        val pass = newUser["password"]
         auth.createUserWithEmailAndPassword(email ?: "", pass ?: "")
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     Log.d("FIREBASE_AUTH_LOG", "createUserWithEmail:success")
                     user = auth.currentUser
+
+                    val uid = user?.uid ?:""
+                    if (uid == "") {
+                        Log.w("FIREBASE_DB_LOG", "Adding User to DB:failure no UID")
+                        Toast.makeText(
+                            baseContext,
+                            "Adding to database failed.",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                        return@addOnCompleteListener
+                    }
+
+                    newUser.set("uid",uid)
+                    db.collection("users").document(uid)
+                        .set(newUser)
+                        .addOnSuccessListener {
+                            Log.w("FIREBASE_DB_LOG", "Adding User to DB:Success")
+                        }
+                        .addOnFailureListener {
+                            Log.w("FIREBASE_DB_LOG", "Adding User to DB:failure", it)
+                            Toast.makeText(
+                                baseContext,
+                                "Adding to database failed.",
+                                Toast.LENGTH_SHORT,
+                            ).show()
+                        }
+
                 } else {
                     // If sign in fails, display a message to the user.
                     Log.w("FIREBASE_AUTH_LOG", "createUserWithEmail:failure", task.exception)
@@ -122,31 +152,6 @@ class RegistrationSecondPart : AppCompatActivity() {
                         Toast.LENGTH_SHORT,
                     ).show()
                 }
-            }
-
-        val uid = user?.uid ?:""
-        if (uid == "") {
-            Log.w("FIREBASE_DB_LOG", "Adding User to DB:failure no UID")
-            Toast.makeText(
-                baseContext,
-                "Adding to database failed.",
-                Toast.LENGTH_SHORT,
-            ).show()
-            return
-        }
-        newUser.set("uid",uid)
-        db.collection("users").document(uid)
-            .set(newUser)
-            .addOnSuccessListener {
-                Log.w("FIREBASE_DB_LOG", "Adding User to DB:Success")
-            }
-            .addOnFailureListener {
-                Log.w("FIREBASE_DB_LOG", "Adding User to DB:failure", it)
-                Toast.makeText(
-                    baseContext,
-                    "Adding to database failed.",
-                    Toast.LENGTH_SHORT,
-                ).show()
             }
     }
 }
