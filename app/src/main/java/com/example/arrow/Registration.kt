099a4134700079dda46aabcad0c2e7248a97824e
E-Museo <e.museongpilipinas@gmail.com>
2023-08-18 21:12:08 +0800

    pacheck

--- app/src/main/java/com/example/arrow/Registration.kt
+++ app/src/main/java/com/example/arrow/Registration.kt
@@ -55,25 +55,17 @@ open class Registration : AppCompatActivity() {
                     startActivity(openReg2)*/
                 }
             } else{
-                if (name.isEmpty()){
-                    Toast.makeText(this, "This field must not be empty.", Toast.LENGTH_SHORT).show()
-                }
-                if (email.isEmpty()){
-                    Toast.makeText(this, "This field must not be empty.", Toast.LENGTH_SHORT).show()
-                }
-                if (pass.isEmpty()){
-                    Toast.makeText(this, "This field must not be empty.", Toast.LENGTH_SHORT).show()
-                }
+                Toast.makeText(this, "All fields must not be empty.", Toast.LENGTH_SHORT).show()
             }
 
             // To check password if valid
-            passwordValidation(pass, email)
+            passwordValidation(pass, email, name)
         }
     }
 
     // To validate input password
     @SuppressLint("ResourceAsColor")
-    private fun passwordValidation(passwordCheck: String, email: String) {
+    private fun passwordValidation(passwordCheck: String, email: String, name: String) {
         val passValidation1 = findViewById<CardView>(R.id.valid1)
         val passValidation2 = findViewById<CardView>(R.id.valid2)
         val passValidation3 = findViewById<CardView>(R.id.valid3)
@@ -103,11 +95,11 @@ open class Registration : AppCompatActivity() {
             passCheck3 = false
         }
 
-        checkAll(email)
+        checkAll(email, name)
     }
 
-    private fun checkAll(email: String){
-        if (passCheck1 && passCheck2 && passCheck3 && email.length >= 8){
+    private fun checkAll(email: String, name: String){
+        if (passCheck1 && passCheck2 && passCheck3 && email.isNotEmpty()  && name.isNotEmpty()){
             canContinue = true
         }
     }
