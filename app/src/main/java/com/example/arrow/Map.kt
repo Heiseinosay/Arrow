acd029e1f8bfc9fb8e3fcebdd33fbac515790c4d
Mark-Asuncion <mark.asuncion.000@gmail.com>
2023-08-19 15:07:07 +0800

    switch to mapbox for vector tile sources; added user location on map (might not work)

--- app/src/main/java/com/example/arrow/Map.kt
+++ app/src/main/java/com/example/arrow/Map.kt
@@ -1,30 +1,69 @@
 package com.example.arrow
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import android.Manifest
-import android.preference.PreferenceManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
+import android.location.Location
+import android.util.Log
+//import android.preference.PreferenceManager
 import androidx.activity.result.ActivityResultLauncher
+import androidx.annotation.DrawableRes
 
-import org.osmdroid.config.Configuration.*
-import org.osmdroid.tileprovider.tilesource.TileSourceFactory
-import org.osmdroid.util.GeoPoint
-import org.osmdroid.views.MapController
-import org.osmdroid.views.MapView
-import org.osmdroid.views.overlay.gestures.RotationGestureOverlay
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.mapbox.android.gestures.MoveGestureDetector
+import com.mapbox.geojson.Point
+import com.mapbox.maps.MapView
+import com.mapbox.maps.Style
+import com.mapbox.maps.CameraBoundsOptions
+import com.mapbox.maps.CameraOptions
+import com.mapbox.maps.CoordinateBounds
+import com.mapbox.maps.FreeCameraOptions
+import com.mapbox.maps.MapboxMap
+import com.mapbox.maps.extension.style.expressions.dsl.generated.interpolate
+import com.mapbox.maps.plugin.LocationPuck2D
+import com.mapbox.maps.plugin.annotation.annotations
+import com.mapbox.maps.plugin.annotation.generated.PointAnnotationOptions
+import com.mapbox.maps.plugin.annotation.generated.createPointAnnotationManager
+import com.mapbox.maps.plugin.gestures.OnMoveListener
+import com.mapbox.maps.plugin.gestures.gestures
+import com.mapbox.maps.plugin.locationcomponent.OnIndicatorBearingChangedListener
+import com.mapbox.maps.plugin.locationcomponent.OnIndicatorPositionChangedListener
+import com.mapbox.maps.plugin.locationcomponent.location
+import java.util.jar.Pack200.Packer
+
+//import org.osmdroid.config.Configuration.*
+//import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+//import org.osmdroid.util.GeoPoint
+//import org.osmdroid.views.MapController
+//import org.osmdroid.views.MapView
+//import org.osmdroid.views.overlay.gestures.RotationGestureOverlay
 
 class Map : AppCompatActivity() {
     lateinit var reqPermissionLauncher: ActivityResultLauncher<Array<String>>
-    private lateinit var map : MapView
-//    private lateinit var mapController : MapController
-    val permissions = arrayOf(
+    // Location provider
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    val PERMISSIONS = arrayOf(
         Manifest.permission.ACCESS_COARSE_LOCATION,
         Manifest.permission.ACCESS_FINE_LOCATION,
         Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        Manifest.permission.INTERNET,
     )
-    var permissionOK: Boolean = false
+    var mapView: MapView? = null
+    var mapboxMap: MapboxMap? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
@@ -35,45 +74,175 @@ class Map : AppCompatActivity() {
             )
             { permissions ->
                 val allPermissionsGranted = permissions.all { it.value }
+                if (permissions[PERMISSIONS[0]] ?: false && permissions[PERMISSIONS[1]] ?: false ) {
+                    setupLocationProvider()
+                }
                 if (allPermissionsGranted) {
                     Toast.makeText( applicationContext, "Permission Granted", Toast.LENGTH_SHORT).show()
-                    permissionOK = true
                 }
                 else {
                     Toast.makeText( applicationContext, "One of Permission is Denied", Toast.LENGTH_SHORT).show()
-                    permissionOK = false
                 }
             }
-        askPermissions()
+        reqPermissionLauncher.launch(PERMISSIONS)
 
-        getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this))
-        map = findViewById<MapView>(R.id.map)
+        mapView = findViewById(R.id.mapView)
+        mapboxMap = mapView?.getMapboxMap()
 
-        // default view
-        val mapController = map.controller
-        mapController.setZoom(20)
-        mapController.setCenter(GeoPoint(14.60195,120.98945))
+        onMapReady()
+    }
+    private fun onMapReady() {
+        mapboxMap?.loadStyleUri(Style.MAPBOX_STREETS,object : Style.OnStyleLoaded {
+            override fun onStyleLoaded(style: Style) {
+                initLocationComponent()
+                setupGesturesListener()
+                val camera = CameraOptions.Builder()
+                    .center(Point.fromLngLat(120.98945,14.60195))
+                    .zoom(15.0)
+                    .build()
+                mapboxMap?.setCamera(camera)
 
-        // add rotation capabilities
-        val rotationGestureOverlay = RotationGestureOverlay(map)
-        rotationGestureOverlay.isEnabled
-        map.setMultiTouchControls(true)
-        map.overlays.add(rotationGestureOverlay)
+                val southwest = Point.fromLngLat(120.98452,14.59990)
+                val northeast = Point.fromLngLat(120.99466,14.60415)
+
+//                val coordBound = CoordinateBounds(southwest,northeast)
+//                val cmBounds: CameraBoundsOptions.Builder = CameraBoundsOptions.Builder()
+//                cmBounds.bounds(coordBound)
+//                mapboxMap?.setBounds(cmBounds.build())
+
+                addAnnotationToMap(southwest.longitude(),southwest.latitude())
+                addAnnotationToMap(northeast.longitude(), northeast.latitude())
+                addAnnotationToMap(camera.center!!.longitude(),camera.center!!.latitude())
+            }
+        })
     }
 
-     override fun onResume() {
-        super.onResume()
-        map.onResume()
+    private fun setupGesturesListener() {
+        mapView?.gestures?.addOnMoveListener(onMoveListener)
     }
 
-    override fun onPause() {
-        super.onPause()
-        map.onPause()
+    private val onIndicatorBearingChangedListener = OnIndicatorBearingChangedListener {
+        mapboxMap?.setCamera(CameraOptions.Builder().bearing(it).build())
+    }
+
+    private val onIndicatorPositionChangedListener = OnIndicatorPositionChangedListener {
+        mapboxMap?.setCamera(CameraOptions.Builder().center(it).build())
+        mapView?.gestures?.focalPoint = mapboxMap?.pixelForCoordinate(it)
+    }
+
+    private val onMoveListener = object : OnMoveListener {
+        override fun onMoveBegin(detector: MoveGestureDetector) {
+            onCameraTrackingDismissed()
+        }
+
+        override fun onMove(detector: MoveGestureDetector): Boolean {
+            return false
+        }
+
+        override fun onMoveEnd(detector: MoveGestureDetector) {}
+    }
+    private fun onCameraTrackingDismissed() {
+        Toast.makeText(this, "onCameraTrackingDismissed", Toast.LENGTH_SHORT).show()
+        mapView?.location?.removeOnIndicatorPositionChangedListener(onIndicatorPositionChangedListener)
+        mapView?.location?.removeOnIndicatorBearingChangedListener(onIndicatorBearingChangedListener)
+        mapView?.gestures?.removeOnMoveListener(onMoveListener)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView?.location?.removeOnIndicatorBearingChangedListener(onIndicatorBearingChangedListener)
+        mapView?.location?.removeOnIndicatorPositionChangedListener(onIndicatorPositionChangedListener)
+        mapView?.gestures?.removeOnMoveListener(onMoveListener)
+    }
+
+    private fun initLocationComponent() {
+        val locationComponentPlugin = mapView?.location
+        locationComponentPlugin?.updateSettings {
+            this.enabled = true
+            this.locationPuck = LocationPuck2D(
+                bearingImage = AppCompatResources.getDrawable(
+                    this@Map,
+                    R.drawable.arrowvector,
+                ),
+                shadowImage = AppCompatResources.getDrawable(
+                    this@Map,
+                    R.drawable.arrowvector,
+                ),
+                scaleExpression = interpolate {
+                    linear()
+                    zoom()
+                    stop {
+                        literal(0.0)
+                        literal(0.6)
+                    }
+                    stop {
+                        literal(20.0)
+                        literal(1.0)
+                    }
+                }.toJson()
+            )
+        }
+        locationComponentPlugin?.addOnIndicatorPositionChangedListener(onIndicatorPositionChangedListener)
+        locationComponentPlugin?.addOnIndicatorBearingChangedListener(onIndicatorBearingChangedListener)
+    }
+    private fun setupLocationProvider() {
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        Log.i("LOCATION_PROVIDER","Initialized")
+
+        if (ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION) !=
+            PackageManager.PERMISSION_GRANTED &&
+            ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) !=
+            PackageManager.PERMISSION_GRANTED) {
+            reqPermissionLauncher.launch(PERMISSIONS)
+            Log.i("LOCATION_PROVIDER","Ask Permission")
+        }
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location : Location? ->
+                if (location != null) {
+                    Log.i("LOCATION_PROVIDER","Latitude: ${location?.latitude} Longtitude: ${location?.longitude}")
+                }
+            }
+    }
+
+    private fun addAnnotationToMap(longtitude: Double, latitude: Double) {
+        bitmapFromDrawableRes(
+            this@Map,
+            R.drawable.arrowvector
+        )?.let {
+            val annotationApi = mapView?.annotations
+            val pointAnnotationManager = annotationApi?.createPointAnnotationManager(mapView!!)
+            val pointAnnotationOptions = PointAnnotationOptions()
+                .withPoint(Point.fromLngLat(longtitude, latitude))
+                .withIconImage(it)
+
+            Toast.makeText(applicationContext,"Drawing Icon",Toast.LENGTH_LONG).show()
+            pointAnnotationManager?.create(pointAnnotationOptions)
+        }
+    }
+    private fun bitmapFromDrawableRes(context: Context, @DrawableRes resourceId: Int) =
+        convertDrawableToBitmap(AppCompatResources.getDrawable(context, resourceId))
+
+    private fun convertDrawableToBitmap(sourceDrawable: Drawable?): Bitmap? {
+        if (sourceDrawable == null) {
+            return null
+        }
+        return if (sourceDrawable is BitmapDrawable) {
+            sourceDrawable.bitmap
+        } else {
+            val constantState = sourceDrawable.constantState ?: return null
+            val drawable = constantState.newDrawable().mutate()
+            val bitmap: Bitmap = Bitmap.createBitmap(
+                drawable.intrinsicWidth, drawable.intrinsicHeight,
+                Bitmap.Config.ARGB_8888
+            )
+            val canvas = Canvas(bitmap)
+            drawable.setBounds(0, 0, canvas.width, canvas.height)
+            drawable.draw(canvas)
+            bitmap
+        }
     }
 
     fun askPermissions() {
-        // TODO: do a check if permission is denied
-
-        reqPermissionLauncher.launch(permissions)
+        reqPermissionLauncher.launch(PERMISSIONS)
     }
 }
