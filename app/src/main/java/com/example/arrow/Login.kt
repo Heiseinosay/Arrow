2a8c85592a3cc78e2f123efbb7205337e1876313
E-Museo <darendasallas27@gmail.com>
2023-08-19 12:27:22 +0800

    login remember me

--- app/src/main/java/com/example/arrow/Login.kt
+++ app/src/main/java/com/example/arrow/Login.kt
@@ -1,5 +1,6 @@
-package com.example.arrow
+    package com.example.arrow
 
+<<<<<<< Updated upstream
 import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
@@ -14,24 +15,82 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class Login : AppCompatActivity() {
+=======
+    import android.content.ContentValues.TAG
+    import android.content.Intent
+    import android.content.SharedPreferences
+    import androidx.appcompat.app.AppCompatActivity
+    import android.os.Bundle
+    import android.util.Log
+    import android.widget.Button
+    import android.widget.EditText
+    import android.widget.TextView
+    import android.widget.Toast
+    import com.google.firebase.auth.FirebaseAuth
+    import com.google.firebase.auth.ktx.auth
+    import com.google.firebase.ktx.Firebase
 
-    private lateinit var auth: FirebaseAuth
+    import android.view.Window
+    import android.view.WindowManager
+    import android.widget.CheckBox
+    import androidx.core.content.ContextCompat
 
+    class Login : AppCompatActivity() {
+>>>>>>> Stashed changes
+
+        private lateinit var auth: FirebaseAuth
+        private lateinit var sharedPreferences: SharedPreferences
+
+<<<<<<< Updated upstream
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         // TODO: connect database and add functional query
 
         auth = Firebase.auth
+=======
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_login)
 
-        // ID'S
-        val forgotPassword = findViewById<TextView>(R.id.tvForgotPassword)
-        val register = findViewById<TextView>(R.id.tvRegister)
-        val loginButton = findViewById<Button>(R.id.btnLogin)
+            // this.onBackPressed ()
+            // SET STATUS BAR COLOR
+            val window: Window = window
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.statusBarColor = ContextCompat.getColor(this, R.color.red)
 
-        forgotPassword.setOnClickListener {
-            Toast.makeText(this, "Nothing here for now!" , Toast.LENGTH_SHORT).show()
+            //Firebase
+            auth = Firebase.auth
+
+            //save email and password
+            sharedPreferences = getSharedPreferences("rememberMe", MODE_PRIVATE)
+            val checkBox = findViewById<CheckBox>(R.id.checkBox)
+            val isChecked = checkBox.isChecked
+
+            if (isChecked){
+                val email = findViewById<EditText>(R.id.inputEmail).text.toString()
+                val password = findViewById<EditText>(R.id.inputPassword).text.toString()
+
+                sharedPreferences.edit().putString("email", email).putString("password", password).apply()
+            }
+>>>>>>> Stashed changes
+
+
+            val forgotPassword = findViewById<TextView>(R.id.tvForgotPassword)
+            val register = findViewById<TextView>(R.id.tvRegister)
+            val loginButton = findViewById<Button>(R.id.btnLogin)
+
+            forgotPassword.setOnClickListener {
+                Toast.makeText(this, "Nothing here for now!" , Toast.LENGTH_SHORT).show()
+            }
+            register.setOnClickListener {
+                val openRegister = Intent(this@Login, Registration::class.java)
+                startActivity(openRegister)
+            }
+            loginButton.setOnClickListener { performLogin() }
+
         }
+<<<<<<< Updated upstream
         register.setOnClickListener {
             val openRegister = Intent(this@Login, Registration::class.java)
             startActivity(openRegister)
@@ -52,30 +111,56 @@ class Login : AppCompatActivity() {
             Toast.makeText(this, "Email or Password field must not be empty.", Toast.LENGTH_SHORT).show()
             return
         }
+=======
+        // GETTING INPUT FROM USE
+        private fun performLogin(){
+            val email = findViewById<EditText>(R.id.inputEmail)
+            val password = findViewById<EditText>(R.id.inputPassword)
 
-        val emailCheck = email.text.toString()
-        val passwordCheck = password.text.toString()
+            // CHECK FOR NULL INPUT
+            if (email.text.isEmpty() || password.text.isEmpty()) {
+                Toast.makeText(this, "Email or Password field must not be empty.", Toast.LENGTH_SHORT).show()
+                return
+            }
 
-        auth.signInWithEmailAndPassword(emailCheck, passwordCheck)
-            .addOnCompleteListener(this) { task ->
+            val emailCheck = email.text.toString()
+            val passwordCheck = password.text.toString()
+>>>>>>> Stashed changes
+
+            auth.signInWithEmailAndPassword(emailCheck, passwordCheck).addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     // Sign in success, update UI with the signed-in user's information
                     Log.d(TAG, "signInWithEmail:success")
                     val user = auth.currentUser
+                    val isChecked = sharedPreferences.getBoolean("rememberMe", false)
 
+<<<<<<< Updated upstream
                     // redirect to Main Activity
                     // nothing here for now
+=======
+                    if (isChecked) {
+                        // redirect to Main Activity
+                        // PROCEED ON BIRDS EYE VIEW
+                        val openBirdsEyeView = Intent(this@Login, BirdsEyeView::class.java)
+                        startActivity(openBirdsEyeView)
+                        finish()
+                    }
+>>>>>>> Stashed changes
                 } else {
                     // If sign in fails, display a message to the user.
                     Log.w(TAG, "signInWithEmail:failure", task.exception)
                     Toast.makeText(
+<<<<<<< Updated upstream
                         baseContext,
                         "Authentication failed.",
                         Toast.LENGTH_SHORT,
                     ).show()
 
                     // nothing here for now
+=======
+                        baseContext, "Authentication failed.", Toast.LENGTH_SHORT,).show()
+>>>>>>> Stashed changes
                 }
             }
+        }
     }
-}
--- app/src/main/java/com/example/arrow/Registration.kt
+++ app/src/main/java/com/example/arrow/Registration.kt
@@ -72,5 +72,115 @@ class Registration : AppCompatActivity() {
                     }
                 }
         }
+<<<<<<< Updated upstream
+=======
+
+        // To check password if valid
+        contChange()
+    }
+
+    // To validate input password
+    @SuppressLint("ResourceAsColor")
+    private fun passwordValidation(name: String, email: String, passwordCheck: String  ) {
+        val passValidation1 = findViewById<CardView>(R.id.valid1)
+        val passValidation2 = findViewById<CardView>(R.id.valid2)
+        val passValidation3 = findViewById<CardView>(R.id.valid3)
+        val passText1 = findViewById<TextView>(R.id.tvValidation1)
+        val passText2 = findViewById<TextView>(R.id.tvValidation2)
+        val passText3 = findViewById<TextView>(R.id.tvValidation3)
+
+        if (passwordCheck.length >= 8){
+            passValidation1.setCardBackgroundColor(ContextCompat.getColor(this, R.color.blue))
+            passText1.setTextColor(ContextCompat.getColor(this,R.color.blue))
+            passCheck1 = true
+        } else {
+            passValidation1.setCardBackgroundColor(ContextCompat.getColor(this, R.color.darkGrey))
+            passText1.setTextColor(ContextCompat.getColor(this,R.color.darkGrey))
+            passCheck1 = false
+
+        }
+
+        if (("^.*[A-Z].*$").toRegex().matches(passwordCheck)){
+            passValidation2.setCardBackgroundColor(ContextCompat.getColor(this, R.color.blue))
+            passText2.setTextColor(ContextCompat.getColor(this,R.color.blue))
+            passCheck2 = true
+        } else {
+            passValidation2.setCardBackgroundColor(ContextCompat.getColor(this, R.color.darkGrey))
+            passText2.setTextColor(ContextCompat.getColor(this,R.color.darkGrey))
+            passCheck2 = false
+        }
+
+        if(("^.*[0-9].*$").toRegex().matches(passwordCheck)){
+            passValidation3.setCardBackgroundColor(ContextCompat.getColor(this, R.color.blue))
+            passText3.setTextColor(ContextCompat.getColor(this,R.color.blue))
+            passCheck3 = true
+        } else {
+            passValidation3.setCardBackgroundColor(ContextCompat.getColor(this, R.color.darkGrey))
+            passText3.setTextColor(ContextCompat.getColor(this,R.color.darkGrey))
+            passCheck3 = false
+        }
+
+        checkAll(name, email)
+    }
+
+    private fun checkAll(name: String, email: String){
+        if (passCheck1 && passCheck2 && passCheck3 && email.isNotEmpty()  && name.isNotEmpty()){
+            canContinue = true
+        } else{
+            canContinue = false
+        }
+    }
+
+    // To constantly check the changes in password edit
+    private fun contChange(){
+        val changedName = findViewById<EditText>(R.id.tvFullName)
+        val changedEmail = findViewById<EditText>(R.id.tvEmail)
+        val changedPassword = findViewById<EditText>(R.id.tvPassword)
+
+        changedName.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                Log.d("TextWatcher", "Password text changed: $s")
+                passwordValidation(changedName.text.toString(), changedEmail.text.toString(), changedPassword.text.toString())
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+        })
+
+        changedEmail.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                Log.d("TextWatcher", "Password text changed: $s")
+                passwordValidation(changedName.text.toString(), changedEmail.text.toString(), changedPassword.text.toString())
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+        })
+
+        changedPassword.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                Log.d("TextWatcher", "Password text changed: $s")
+                passwordValidation(changedName.text.toString(), changedEmail.text.toString(), changedPassword.text.toString())
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+        })
+>>>>>>> Stashed changes
     }
 }
