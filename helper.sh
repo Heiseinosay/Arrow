b23d271d634a595c906155c0b96199e2a817d5bc
Mark-Asuncion <mark.asuncion.000@gmail.com>
2023-08-25 16:27:51 +0800

    mapbox tiling service

---
+++ helper.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+if [ -z "$MAPBOX_ACCESS_TOKEN" ]; then
+    echo 'no MAPBOX_ACCESS_TOKEN environment set'
+    exit 1
+fi
+
+if [ "$1" == "get" ]; then
+    curl -O "https://api.mapbox.com/v4/mapbox.mapbox-streets-v8/16/54793/30080.mvt?style=mapbox://styles/mapbox/streets-v12@00&access_token=$MAPBOX_ACCESS_TOKEN"
+elif [ "$1" == "delete" ]; then
+    if [ -n "$2" ]; then
+        curl -X DELETE "https://api.mapbox.com/tilesets/v1/sources/mark-asuncion/${1}?access_token=${MAPBOX_ACCESS_TOKEN}"
+        echo "$(date):: deleting: ${2}" | tee -a arrow.log
+    fi
+elif [ "$1" == "list" ]; then
+    tilesets list mark-asuncion
+elif [ "$1" == "upload" ]; then
+    if [[ -n $2 && -n $3 ]]; then
+        tilesets upload-source mark-asuncion "$2" "$3"
+        echo "$(date):: upload with name: ${2}" | tee -a arrow.log
+    fi
+elif [ "$1" == "create" ]; then
+    if [[ -n $2 && -n $3 ]]; then
+        tilesets create mark-asuncion."$2" --recipe "$3" --name "$2"
+        echo "$(date):: creating recipe with name: ${2} from ${3}" | tee -a arrow.log
+    fi
+elif [ "$1" == "publish" ]; then
+    if [[ -n $2 ]]; then
+        tilesets publish mark-asuncion."$2"
+        echo "$(date):: publishing with name: ${2}" | tee -a arrow.log
+    fi
+fi
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Requirements
+# Coordinates of UE
+```
+#18.16/14.601877/120.990027
+```
+## mapbox-tilesets cli
+    * to install run ```pip install mapbox-tilesets```
+# Tileset ID of UE bounds
+    * ```16/54793/30080```
